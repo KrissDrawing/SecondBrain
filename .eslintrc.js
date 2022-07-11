f4bf5842d47203c93446a3802a408659d9107c00
krzysztof ziecina <kziecina@airnauts.com>
2022-07-11 19:30:06 +0200

    feat: add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	root: true,
+	extends: [
+		'airbnb',
+		'airbnb-typescript',
+		'airbnb/hooks',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { project: ['./tsconfig.json'] },
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['src/**/*.test.ts', 'src/frontend/generated/*'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/no-unstable-nested-components': 'off',
+		'@typescript-eslint/no-use-before-define': 'off',
+	},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  singleAttributePerLine: false,
+};
